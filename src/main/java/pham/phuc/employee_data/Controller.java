05e697e7f7f979dcf687faffc950a26be26b7d48
Acus <acus1304pgphcm@gmail.com>
2022-08-16 20:30:13 +0700

    Update the JavaFX files: hello-view.fxml and add-view.fxml.

--- src/main/java/pham/phuc/employee_data/Controller.java
+++ src/main/java/pham/phuc/employee_data/Controller.java
@@ -1,14 +1,45 @@
 package pham.phuc.employee_data;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Controller {
     @FXML
-    private Label welcomeText;
+    private Label welcomeText0;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText0.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    public void onAddButtonClick(ActionEvent event) throws IOException {
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(Controller.class.getResource("add-view.fxml"));
+        Parent addParent = loader.load();
+        Scene addScene = new Scene(addParent);
+        stage.setScene(addScene);
+    }
+
+    @FXML
+    public void onExitButtonCLick (ActionEvent event) {
+        Platform.exit();
+    }
+
+    public void onAddReturnClick(ActionEvent event) throws IOException {
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(Controller.class.getResource("hello-view.fxml"));
+        Parent addParent = loader.load();
+        Scene addScene = new Scene(addParent);
+        stage.setScene(addScene);
     }
 }
