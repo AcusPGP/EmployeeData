ac1ef8cc0e5867e8d487377e833991e932f325ed
Son Vu <namson0482@gmail.com>
2022-08-24 09:40:08 +0700

    give some examples of thread in java

--- src/main/java/employee/pojo/Worker.java
+++ src/main/java/employee/pojo/Worker.java
@@ -1,6 +1,8 @@
 package employee.pojo;
 
 public class Worker extends Person {
+
+
     public Worker(int id, String name, int age, String address, String type, String level) {
         super(id, name, age, address, type, level);
     }
